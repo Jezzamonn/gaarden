039d6c9b0943b26aa2a12d68c2f52fe759743517
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2019-10-04 18:10:47 -0700

    Clean up some unneeded stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,27 @@
 const path = require('path');
 const webpack = require('webpack');
-const nodeExternals = require('webpack-node-externals');
-
-const common = {
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['env']
-                }
-            }
-        ]
-    },
-    stats: {
-        colors: true
-    },
-}
-
-const client = {
-    entry: './src/js/main.js',
-    output: {
-        path: path.resolve(__dirname, 'build/web/js'),
-        filename: 'main.bundle.js'
-    },
-    devtool: 'source-map'
-}
-
-const gifExport = {
-    entry: './src/js/save-frames.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'save-frames.bundle.js'
-    },
-    target: 'node',
-    externals: [nodeExternals()]
-}
 
 module.exports = [
-    Object.assign({}, common, client),
-    Object.assign({}, common, gifExport)
+    {
+        module: {
+            loaders: [
+                {
+                    test: /\.js$/,
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['env']
+                    }
+                }
+            ]
+        },
+        stats: {
+            colors: true
+        },
+        entry: './src/js/main.js',
+        output: {
+            path: path.resolve(__dirname, 'build/web/js'),
+            filename: 'main.bundle.js'
+        },
+        devtool: 'source-map'
+    },
 ];
