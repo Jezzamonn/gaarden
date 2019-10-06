5570303708cda6186e96b805d191dea122a8f240
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2019-10-06 14:31:51 -0700

    Got the house sound sounding nice

--- src/js/controller.js
+++ src/js/controller.js
@@ -58,7 +58,7 @@ export default class Controller {
 
 		// HACK for quickly debugging
 		if (this.random.bool(0.1)) {
-			// this.clickRandomEntity();
+			this.clickRandomEntity();
 		}
 
 		this.musicThing.update(dt);
