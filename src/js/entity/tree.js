f594df160b79d4929ce8394ae82ecdd3f833a448
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2019-10-05 22:13:39 -0700

    Add wind or something

--- src/js/entity/tree.js
+++ src/js/entity/tree.js
@@ -16,6 +16,15 @@ export class Tree extends Entity {
             {x: 0, y: -40},
             {x: 10, y: 0},
         ], {closed: true});
+
+        this.windCount = 0;
+        this.windSpeed = controller.random.real(5, 5.1);
+    }
+
+    update(dt) {
+        super.update(dt);
+
+        this.windCount += dt;
     }
 
     firstUpdate() {
@@ -26,7 +35,8 @@ export class Tree extends Entity {
      * @param {CanvasRenderingContext2D} context 
      */
     localRender(context) {
-        context.transform(1, 0, this.controller.random.real(-0.1, 0.1), 1, 0, 0);
+        const wind = 0.02 * Math.sin(2 * Math.PI * this.windCount / this.windSpeed);
+        context.transform(1, 0, wind, 1, 0, 0);
         super.localRender(context);
     }
 
